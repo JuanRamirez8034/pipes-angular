a4a6003dd1986c82f2702dcf8a8a7a8f5ea5a00e
JuanRamirez <juanramirez8034@gmail.com>
2023-01-05 18:41:46 -0400

    Importado y utilizado componentes basicos de 'primengNG'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// importando los botones del PrimeNG
+import {ButtonModule as primegButtonModule} from 'primeng/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    primegButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
